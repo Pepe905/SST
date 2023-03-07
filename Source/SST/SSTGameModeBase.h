3aa9894834ec4423353a4d5ae8f05daf5428d7e2
Pepe905 <marcelpospiech@googlemail.com>
2023-03-07 15:41:47 +0100

    [*] Project init

---
+++ Source/SST/SSTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SSTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SST_API ASSTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
